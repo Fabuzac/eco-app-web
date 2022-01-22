6203f693e97ca2697d3dff65b0ee8607db8f669d
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-01-22 13:23:44 +0100

    New config, connection attempt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="justify-content-center text-center">
             <h1 class="text-light">Earth-Health</h1>
-            <h2>Dashboard</h2>       
+            <h2>Dashboard </h2>       
             <p>                
                 <iframe width="100%" height="600px" frameborder="0" allowfullscreen src="https://framacarte.org/fr/map/carte-sans-nom_106607?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe>
                 <p><a href="https://framacarte.org/fr/map/new/">Voir en plein écran</a></p>
