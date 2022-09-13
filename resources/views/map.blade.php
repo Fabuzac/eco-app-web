7049a58d47ebb9ef96dfd361a23062dd4c71969a
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-09-13 15:13:12 +0200

    security update

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- HAVE TO CHANGE IN DIV --}}
-{{-- JUST FOR HAVE DATA --}}
 <body>    
     <div class="container">
         <div id='map'></div>  
-        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1IIa6CP8swQaJifuSScfud-7by6CR6KaC&ehbc=2E312F" width="640" height="480"></iframe>
     </div>
 </body>
 @endsection
